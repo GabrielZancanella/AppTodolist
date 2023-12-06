5aca05250f196c994962f5d940df803f2262814b
Gabriel Zancanella Pereira <gabrielzancanella35@gmail.com>
2023-12-06 11:59:59 -0300

    Initialize repo
    Add archives.

---
+++ src/main/java/com/AppTodolist/AppTodolist/AppTodolistApplication.java
@@ -0,0 +1,13 @@
+package com.AppTodolist.AppTodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppTodolistApplication.class, args);
+	}
+
+}
